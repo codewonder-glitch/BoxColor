d9ffb097dfaa434e1fbc096feb1a3470490585fe
Visa <visalakshi.cse@gmail.com>
2020-03-26 22:02:07 -0500

    Initial commit

---
+++ boxcolor/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App {
+  return(){
+  render (
+    <div className="App">
+      
+    </div>
+  );
+}}
+
+export default App;
