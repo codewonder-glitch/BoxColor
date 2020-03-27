a98c3d8fb6465cb2e03ccc14d476a3b831daa013
Visa <visalakshi.cse@gmail.com>
2020-03-27 01:15:13 -0500

    Formatting done

--- boxcolor/src/App.js
+++ boxcolor/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 class App extends React.Component{
   constructor(props) {
     super(props);
-
     this.state = {
       color: 'blue',
       clickCounter: 0
@@ -14,23 +13,21 @@ class App extends React.Component{
   }
   onChange = (e) => {
     this.setState((prevstate)=>{
-
-     return( {clickCounter: prevstate.clickCounter++,
-      color:(prevstate.color==='blue')?'orange':'blue'});
+    return( {clickCounter: prevstate.clickCounter++,
+    color:(prevstate.color==='blue')?'orange':'blue'});
     });
     console.log("click counter ===> ", this.state.clickCounter)
   }
   render(){
-
   return (
     <div className="App">
-       <h1> Box color change .</h1>
-        <div              className="colorBox"
-        style={{backgroundColor: this.state.color}}
-        onClick={this.onChange}
-        >
-        <p>Click Here</p>
-        </div>
+      <h1> Box color change .</h1>
+      <div className="colorBox"
+      style={{backgroundColor: this.state.color}}
+      onClick={this.onChange}
+      >
+      <p>Click Here</p>
+      </div>
     </div>
   );
 }}
