5fa2ff42c12ac7cde0d311e89c3db9028e86e6ee
Visa <visalakshi.cse@gmail.com>
2020-03-27 01:32:48 -0500

    Pseudocode written

---
+++ boxcolor/Pseudocode/Pseudocode.js
@@ -0,0 +1,13 @@
+Objective
+
+Create a <div> box with an onClick handle event that changes the background color when clicked. 
+When the box is clicked, we want a clickCounter indicating how many times it was clicked. 
+We want the box color to toggle back to it's original color! (Hint: create a toggle function)
+
+
+
+Updated functional component to a class component (App.js)
+Set state ( color & clickCounter)
+When the button is clicked onChange function called where setstate function is used to modify the counter 
+value 
+Ternary operator is used to toggle between blue and orange color
