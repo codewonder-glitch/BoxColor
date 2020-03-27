311f844882e60ae353cbbd3bc2d52a8fefbed084
Visa <visalakshi.cse@gmail.com>
2020-03-27 01:10:30 -0500

    added this.setstate function

--- boxcolor/src/App.js
+++ boxcolor/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './App.css';
 
-class App {
-  return(){
-  render (
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: 'blue',
+      clickCounter: 0
+    }
+  }
+  onChange = (e) => {
+    this.setState((prevstate)=>{
+
+     return( {clickCounter: prevstate.clickCounter++,
+      color:(prevstate.color==='blue')?'orange':'blue'});
+    });
+    console.log("click counter ===> ", this.state.clickCounter)
+  }
+  render(){
+
+  return (
     <div className="App">
-      
+       <h1> Box color change .</h1>
+        <div              className="colorBox"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.onChange}
+        >
+        <p>Click Here</p>
+        </div>
     </div>
   );
 }}
